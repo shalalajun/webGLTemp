062dc1e0fa4e8add951437cccecfa0a6b4c5b708
eunkyo <eunkyo@eunkyoui-Mac-Studio.local>
2022-07-18 11:38:56 +0900

    matrixfirst

--- transformBasic/main.js
+++ transformBasic/main.js
@@ -83,8 +83,9 @@ function main()
     var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
     //gl.bindAttribLocation(program, 0, "a_position"); -> 필요없을까?
     var u_location = gl.getUniformLocation(program, "u_resolution");
-    var matrixLocation = gl.getUniformLocation(program, "u_matrix");
-  
+    var u_transLocation = gl.getUniformLocation(program, "u_transLocation");
+    var rotationLocation = gl.getUniformLocation(program,"u_rotation");
+    var scaleLocation = gl.getUniformLocation(program, "u_scale");
 
 
     // 어트리뷰트등 속성을 넣기 위한 버퍼를 생성, 버퍼셋업
@@ -94,9 +95,9 @@ function main()
     
     setGeometry(gl);
 
-    var translation = [600, 350];
-    var angleInRadians = 10;
-    var scale = [12,1];//스케일
+    var translation = [100,300]; // 위치변환하는 곳
+    var rotation = [0,1];// 회전하는 곳
+    var scale = [1,1];//스케일
    
     drawScene();
 
@@ -130,19 +131,10 @@ function main()
 
 
       gl.uniform2f(u_location, gl.canvas.width, gl.canvas.height); // set the resolution
-      
-      
-      var translationMatrix = m3.translation(translation[0], translation[1]);
-      var rotationMatrix = m3.rotation(angleInRadians);
-      var scaleMatrix = m3.scaling(scale[0], scale[1]);
-
-         // Multiply the matrices.
-      var matrix = m3.multiply(translationMatrix, rotationMatrix);
-      matrix = m3.multiply(matrix, scaleMatrix);
-
-      // Set the matrix.
-      gl.uniformMatrix3fv(matrixLocation, false, matrix);
-
+      gl.uniform2fv(u_transLocation, translation); // Set the translation.
+      gl.uniform2fv(rotationLocation, rotation);
+      gl.uniform2fv(scaleLocation, scale);
+    
       // 그리는 부분
       var primitiveType = gl.TRIANGLES;
       var offset = 0;
@@ -151,103 +143,71 @@ function main()
     
     
     }
-  
-}
-
-var m3 = {
-  translation: function(tx, ty) {
-    return [
-      1, 0, 0,
-      0, 1, 0,
-      tx, ty, 1,
-    ];
-  },
-
-  rotation: function(angleInRadians) {
-    var c = Math.cos(angleInRadians);
-    var s = Math.sin(angleInRadians);
-    return [
-      c,-s, 0,
-      s, c, 0,
-      0, 0, 1,
-    ];
-  },
-
-  scaling: function(sx, sy) {
-    return [
-      sx, 0, 0,
-      0, sy, 0,
-      0, 0, 1,
-    ];
-  },
-
-  multiply: function(a, b) {
-    var a00 = a[0 * 3 + 0];
-    var a01 = a[0 * 3 + 1];
-    var a02 = a[0 * 3 + 2];
-    var a10 = a[1 * 3 + 0];
-    var a11 = a[1 * 3 + 1];
-    var a12 = a[1 * 3 + 2];
-    var a20 = a[2 * 3 + 0];
-    var a21 = a[2 * 3 + 1];
-    var a22 = a[2 * 3 + 2];
-    var b00 = b[0 * 3 + 0];
-    var b01 = b[0 * 3 + 1];
-    var b02 = b[0 * 3 + 2];
-    var b10 = b[1 * 3 + 0];
-    var b11 = b[1 * 3 + 1];
-    var b12 = b[1 * 3 + 2];
-    var b20 = b[2 * 3 + 0];
-    var b21 = b[2 * 3 + 1];
-    var b22 = b[2 * 3 + 2];
-    return [
-      b00 * a00 + b01 * a10 + b02 * a20,
-      b00 * a01 + b01 * a11 + b02 * a21,
-      b00 * a02 + b01 * a12 + b02 * a22,
-      b10 * a00 + b11 * a10 + b12 * a20,
-      b10 * a01 + b11 * a11 + b12 * a21,
-      b10 * a02 + b11 * a12 + b12 * a22,
-      b20 * a00 + b21 * a10 + b22 * a20,
-      b20 * a01 + b21 * a11 + b22 * a21,
-      b20 * a02 + b21 * a12 + b22 * a22,
-    ];
-  },
-}
-
-
-function setGeometry(gl) {
-  gl.bufferData(
-    gl.ARRAY_BUFFER,
-    new Float32Array([
-      // 왼쪽 열
-      0, 0,
-      30, 0,
-      0, 150,
-      0, 150,
-      30, 0,
-      30, 150,
- 
-      // 상단 가로 획
-      30, 0,
-      100, 0,
-      30, 30,
-      30, 30,
-      100, 0,
-      100, 30,
- 
-      // 중간 가로 획
-      30, 60,
-      67, 60,
-      30, 90,
-      30, 90,
-      67, 60,
-      67, 90,
-    ]),
-    gl.STATIC_DRAW
-  );
+    
+    
+    function setGeometry(gl) {
+      gl.bufferData(
+        gl.ARRAY_BUFFER,
+        new Float32Array([
+          // 왼쪽 열
+          0, 0,
+          30, 0,
+          0, 150,
+          0, 150,
+          30, 0,
+          30, 150,
+     
+          // 상단 가로 획
+          30, 0,
+          100, 0,
+          30, 30,
+          30, 30,
+          100, 0,
+          100, 30,
+     
+          // 중간 가로 획
+          30, 60,
+          67, 60,
+          30, 90,
+          30, 90,
+          67, 60,
+          67, 90,
+        ]),
+        gl.STATIC_DRAW
+      );
+    }
 }
 
 main();
 
 
 
+var m3 = {
+  translation: function(tx, ty)
+  {
+    return
+    [
+      1,  0, 0,
+      0,  1, 0,
+     tx, ty, 1,
+    ];
+  },
+
+  rotation: function(angleInRadians)
+  {
+    return[
+      c,-s, 0,
+      s, c, 0,
+      0, 0, 1,
+    ]
+  },
+
+  scaling: function(sx, sy) {
+    return [
+      sx, 0, 0,
+      0, sy, 0,
+      0,  0, 1,
+    ];
+  },
+
+};
